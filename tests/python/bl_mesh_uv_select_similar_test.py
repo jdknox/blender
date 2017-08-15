1df1c90c657ad1c9c760990bbac88ce825aa2f27
Jeff Knox <jd.knox@gmail.com>
2017-08-14 20:56:40 -0700

    add single UV face compare tests and multiple selection tests

--- tests/python/bl_mesh_uv_select_similar_test.py
+++ tests/python/bl_mesh_uv_select_similar_test.py
@@ -95,11 +95,21 @@ class TestHelper:
     def select_all_mesh_faces(self):
         bpy.ops.mesh.select_all(action='SELECT')
 
-    def select_uv_face(self, index):
-        for loop in self.bmesh.faces[index].loops:
-            loop[self.uv_layer].select = True
+    def deselect_all_uv_faces(self):
+        for face in self.bmesh.faces:
+            for loop in face.loops:
+                loop[self.uv_layer].select = False
+
+    def select_uv_faces(self, indices):
+        # rough check for non-sequence
+        if not hasattr(indices, '__len__'):
+            indices = [indices]
+            
+        for index in indices:
+            for loop in self.bmesh.faces[index].loops:
+                loop[self.uv_layer].select = True
         bmesh.update_edit_mesh(self.mesh)
-    
+
     def get_selected_uv_faces(self):
         selected_uv_faces = []
         for face in self.bmesh.faces:
@@ -115,23 +125,47 @@ class SelectSimilarUVFaceTest(TestHelper, unittest.TestCase):
     def setUp(self):
         bpy.context.scene.tool_settings.uv_select_mode = 'FACE'
         self.optype = 'AREA'
-    
-    def choose_index_given_all_uvs_visible(self, index, compare='EQUAL', threshold=0.099):
+
+    def reset_all(self):
         self.select_all_mesh_faces()
-        self.select_uv_face(index)
+        self.deselect_all_uv_faces()
+
+    def choose_index_given_all_uvs_visible(self, index, compare, threshold):
+        self.reset_all()
+        self.select_uv_faces(index)
         return bpy.ops.uv.select_similar(type=self.optype, compare=compare, threshold=threshold)
 
-    def test_single_equal(self):
-        res = self.choose_index_given_all_uvs_visible(0)
+    def check_by_index(self, index, should_be, compare='EQUAL', threshold=0.099):
+        res = self.choose_index_given_all_uvs_visible(index, compare=compare, threshold=threshold)
         self.assertEqual({'FINISHED'}, res)
-        selected_uv_faces = self.get_selected_uv_faces()
-        self.assertListEqual(selected_uv_faces, list(range(NUM_FACES)))
+        selected_uv_faces = set(self.get_selected_uv_faces())
+        self.assertSetEqual(selected_uv_faces, should_be)
+
+    def test_single_equal(self):
+        self.check_by_index(0, set(range(NUM_FACES)))
+        self.check_by_index(0, set(range(NUM_FACES * 2)), threshold=0.101)
 
     def test_single_less(self):
-        res = self.choose_index_given_all_uvs_visible(0, compare='LESS')
-        self.assertEqual({'FINISHED'}, res)
-        selected_uv_faces = self.get_selected_uv_faces()
-        self.assertListEqual(selected_uv_faces, list(range(NUM_FACES * NUM_FACES)))
+        self.check_by_index(0, set(range(NUM_FACES * NUM_FACES)), compare='LESS')
+        index = NUM_FACES * (NUM_FACES - 1)
+        self.check_by_index(index, set(range(index, NUM_FACES * NUM_FACES)), compare='LESS')
+        self.check_by_index(0, set(range(NUM_FACES * NUM_FACES)), compare='LESS', threshold=0.101)
+
+    def test_single_greater(self):
+        self.check_by_index(8, set(range(NUM_FACES * 2)), compare='GREATER')
+        index = NUM_FACES * NUM_FACES - 1
+        self.check_by_index(index, set(range(NUM_FACES * NUM_FACES)), compare='GREATER')
+        self.check_by_index(8, set(range(NUM_FACES * 3)), compare='GREATER', threshold=0.101)
+
+    def test_multiple(self):
+        to_select = range(0, NUM_FACES * NUM_FACES, NUM_FACES)
+        should_be = set(range(NUM_FACES * NUM_FACES))
+        self.check_by_index(to_select, should_be)
+        self.check_by_index(to_select, should_be, compare='LESS')
+        self.check_by_index(to_select, should_be, compare='GREATER')
+        to_select = range(0, NUM_FACES * NUM_FACES)
+        should_be = set(to_select)
+        self.check_by_index(to_select, should_be)
 
 
 def main():
@@ -145,13 +179,13 @@ def main():
 
     parser = argparse.ArgumentParser()
     args, remaining = parser.parse_known_args(argv)
-    
-    unittest.main(argv=remaining)
+
     try:
         unittest.main(argv=remaining)
-    except SystemExit:
+    except SystemExit as e:
+        # keep blender open for debugging tests
         if '--background' in sys.argv:
-            raise(SystemExit)
+            raise(SystemExit(e.code))
 
 
 if __name__ == "__main__":
